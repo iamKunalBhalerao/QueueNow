c4c7746b8d55556a03c25d527dcd83b52286f7f0
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2026-08-21 22:03:00 +0530

    update middleware of surface app to fulfill the claudflare build error

--- apps/surface/src/middleware.ts
+++ apps/surface/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 const PROTECTED_ROUTES = [
   "/dashboard",
